ba112898111e3825c3bdc3cae4f2c2829883400f
Mykola Kovtun <nckkvtn@gmail.com>
2019-04-23 13:39:41 +0300

    generate button which saves zip with xml files

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
-import "./Header.css";
+import React, { Component } from 'react';
+import axios from 'axios';
+import fileDownload from 'js-file-download';
+import './Header.css';
 
 class Header extends Component {
   uploadFile = () => this.fileInput.click();
+
+  downloadOutput = () => {
+    axios.get('/api/generator', { responseType: 'blob' }).then((res) => {
+      fileDownload(res.data, 'output.zip');
+    });
+  };
 
   render() {
     const { fileHandler } = this.props;
@@ -10,20 +18,16 @@ class Header extends Component {
       <div>
         <div className="navigation">
           <div className="logoBlock">
-            <img src={require("../../img/logotype.png")} alt="Logo" />
+            <img src={require('../../img/logotype.png')} alt="Logo" />
             <p>Bounding boxes tool</p>
           </div>
           <nav className="tools">
             <ul>
-              <li
-                title="Upload"
-                className="fas fa-upload"
-                onClick={() => this.uploadFile()}
-              >
+              <li title="Upload" className="fas fa-upload" onClick={() => this.uploadFile()}>
                 <span>Upload</span>
               </li>
-              <li title="Download" className="fa fa-download">
-                <span>Download</span>
+              <li title="Download" className="fa fa-download" onClick={this.downloadOutput}>
+                <span>Genarate</span>
               </li>
               <li title="Prev" className="fas fa-arrow-circle-left" />
               <li title="Next" className="fas fa-arrow-circle-right" />
