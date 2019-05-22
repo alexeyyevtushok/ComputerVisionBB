fc5750d3449e6ce30614680fa26a6f711d633cd6
Mykola Kovtun <nckkvtn@gmail.com>
2019-05-22 09:50:27 +0300

    new client id for google

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import fileDownload from 'js-file-download';
-import { withRouter } from 'react-router-dom';
-import './Header.css';
-import { connect } from 'react-redux';
-import { GoogleLogin } from 'react-google-login';
-import { addImages, resetAll, addGDriveImages } from '../../actions/imagesActions';
+import React, { Component } from "react";
+import axios from "axios";
+import fileDownload from "js-file-download";
+import { withRouter } from "react-router-dom";
+import "./Header.css";
+import { connect } from "react-redux";
+import { GoogleLogin } from "react-google-login";
+import {
+  addImages,
+  resetAll,
+  addGDriveImages,
+} from "../../actions/imagesActions";
 
 class Header extends Component {
   uploadFile = () => this.fileInput.click();
 
   downloadOutput = () => {
-    axios.get('/api/generator', { responseType: 'blob' }).then((res) => {
-      const fileName = prompt('Enter file name:', 'output');
+    axios.get("/api/generator", { responseType: "blob" }).then(res => {
+      const fileName = prompt("Enter file name:", "output");
       if (fileName !== null) {
         fileDownload(res.data, `${fileName}.zip`);
       }
     });
   };
 
-  uploadFileRequest = (event) => {
+  uploadFileRequest = event => {
     this.props.addImages(event.target.files);
   };
 
-  successResponseGoogle = (response) => {
+  successResponseGoogle = response => {
     const { accessToken } = response;
     console.log(accessToken);
-    const url = prompt('Enter link to the folder:');
+    const url = prompt("Enter link to the folder:");
     if (url !== null) {
       this.props.addGDriveImages({ url, accessToken });
     }
   };
 
-  errorResponseGoogle = (response) => {
+  errorResponseGoogle = response => {
     // handle error
     console.log(response);
   };
 
-  reset = (e) => {
-    if (window.confirm('Are you sure?')) {
+  reset = e => {
+    if (window.confirm("Are you sure?")) {
       this.props.resetAll();
     } else {
       e.preventDefault();
@@ -50,18 +54,26 @@ class Header extends Component {
       <div>
         <div className="navigation">
           <div className="logoBlock">
-            <img src={require('../../img/logotype.png')} alt="Logo" />
+            <img src={require("../../img/logotype.png")} alt="Logo" />
             <p>Bounding boxes tool</p>
           </div>
           <nav className="tools">
             <ul>
-              <li title="Upload" className="fab fa-google-drive" style={{ color: '#1967d2' }}>
+              <li
+                title="Upload"
+                className="fab fa-google-drive"
+                style={{ color: "#1967d2" }}
+              >
                 {/* change this to simple span with onClick when registered and check token expiration */}
                 <GoogleLogin
-                  clientId="504294961504-265csqd1aar75cphf1qme0q0ockt8ukt.apps.googleusercontent.com"
+                  // replace hardcoded value
+                  clientId="504294961504-676h7nu5b43s76ph3e3g85bgjjr9dmfl.apps.googleusercontent.com"
                   render={renderProps => (
-                    <span onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                      {'Upload from GDrive'}
+                    <span
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      {"Upload from GDrive"}
                     </span>
                   )}
                   buttonText="Login"
@@ -70,10 +82,18 @@ class Header extends Component {
                   cookiePolicy="single_host_origin"
                 />
               </li>
-              <li title="Upload" className="fas fa-upload" onClick={e => this.uploadFile(e)}>
+              <li
+                title="Upload"
+                className="fas fa-upload"
+                onClick={e => this.uploadFile(e)}
+              >
                 <span>Upload from disk</span>
               </li>
-              <li title="Download" className="fa fa-download" onClick={this.downloadOutput}>
+              <li
+                title="Download"
+                className="fa fa-download"
+                onClick={this.downloadOutput}
+              >
                 <span>Genarate</span>
               </li>
               <a
@@ -106,5 +126,5 @@ export default connect(
     addImages,
     resetAll,
     addGDriveImages,
-  },
+  }
 )(withRouter(Header));
